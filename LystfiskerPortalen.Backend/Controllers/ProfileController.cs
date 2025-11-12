fe2f44a545d6236a6b2a0efe0de1908e2d721845
Kristoffer Heby <kristofferheby@gmail.com>
2025-11-12 18:55:32 +0100

    Added IProfileRepository and ProfileController

---
+++ LystfiskerPortalen.Backend/Controllers/ProfileController.cs
@@ -0,0 +1,34 @@
+﻿using LystfiskerPortalen.Backend.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using RazorClassLibrary;
+
+namespace LystfiskerPortalen.Backend.Controllers
+{
+    public class ProfileController : Controller, IProfileRepository
+    {
+        public void AddAsync(Profile profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Profile>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Profile?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAsync(Profile profile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
