5108ef63b86f01fdc7ea0aa050fd8f81836fb5ae
Kristoffer Heby <kristofferheby@gmail.com>
2025-11-12 13:13:11 +0100

    Added IProfileRepository

---
+++ LystfiskerPortalen.Backend/Persistence/IProfileRepository.cs
@@ -0,0 +1,13 @@
+﻿using RazorClassLibrary;
+
+namespace LystfiskerPortalen.Backend.Persistence
+{
+    public interface IProfileRepository
+    {
+        public Task<List<Profile>> GetAllAsync();
+        public Task<Profile?> GetByIdAsync(int id);
+        public void AddAsync(Profile profile);
+        public void DeleteAsync(int Id);
+        public void UpdateAsync(Profile profile);
+    }
+}
