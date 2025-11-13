10a0321ae1ace0aa6e725101b086c62524f2358c
Kristoffer Heby <kristofferheby@gmail.com>
2025-11-13 13:52:33 +0100

    Fixed review change request

--- LystfiskerPortalen.Backend/Controllers/ProfileController.cs
+++ LystfiskerPortalen.Backend/Controllers/ProfileController.cs
@@ -11,26 +11,36 @@ namespace LystfiskerPortalen.Backend.Controllers
         {
             this.repository = repository;
         }
+
+        [HttpPost]
+        [Route("/Profile")]
         public void AddAsync(Profile profile)
         {
             repository.AddAsync(profile);
         }
 
+        [HttpDelete]
+        [Route("/Profile{id}")]
         public void DeleteAsync(int Id)
         {
             repository?.DeleteAsync(Id);
         }
 
+        [HttpGet]
+        [Route("/Profile")]
         public Task<List<Profile>> GetAllAsync()
         {
             return repository.GetAllAsync();
         }
-
+        [HttpGet]
+        [Route("/Profile{id}")]
         public Task<Profile?> GetByIdAsync(int id)
         {
             return repository.GetByIdAsync(id);
         }
 
+        [HttpPut]
+        [Route("/Profile")]
         public void UpdateAsync(Profile profile)
         {
             repository.UpdateAsync(profile);
