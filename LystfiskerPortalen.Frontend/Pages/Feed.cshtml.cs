68ce8af0cd80cbaaf01fddcff8c75347075798d1
Hamderdenvanvittige <mrmanco17@gmail.com>
2025-11-14 11:18:58 +0100

    outline of the feed page (more work needed)

---
+++ LystfiskerPortalen.Frontend/Pages/Feed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LystfiskerPortalen.Frontend.Pages
+{
+    public class FeedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
