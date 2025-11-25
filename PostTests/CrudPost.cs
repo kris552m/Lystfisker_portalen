479b9092691b30b9cbd6a2928e1e23598baa541b
Nikita Alicia Sparham <nasp67241@edu.ucl.dk>
2025-11-25 09:56:17 +0100

    trying to addLocation

--- PostTests/CrudPost.cs
+++ PostTests/CrudPost.cs
@@ -1,5 +1,6 @@
 using LystfiskerPortalen.Data;
 using LystfiskerPortalen.Models;
+using LystfiskerPortalen.Persistence;
 using Microsoft.EntityFrameworkCore;
 
 namespace PostTests
@@ -17,7 +18,7 @@ namespace PostTests
             .Options;
         }
         [TestMethod]
-        public async Task CanAddProfileToRealDatabase()
+        public async Task CanAddProfileToDatabase()
         {
             using var context = new LystFiskerContext(_options);
             // Arrange: create a profile
@@ -42,57 +43,73 @@ namespace PostTests
 
 
         [TestMethod]
-        public async Task CanGetProfileThrowRepoToRealDatabase()
+        public async Task CanGetProfileThroughRepoFromDatabase()
         {
             using var context = new LystFiskerContext(_options);
-            // Arrange: create a profile
-            var profile = new Profile
-            {
-                Id = Guid.NewGuid().ToString(),
-                UserName = "testuser4",
-                Email = "test@example.com"
-            };
-            context.Users.Add(profile);
-            await context.SaveChangesAsync();
+            ProfileRepository Profilerepo = new ProfileRepository(context);
+
+
 
             // Assert: query back from the real DB
-            var savedProfile = context.Users
-            .Where(u => u.UserName == "testuser4")
-            .Select(u => new { u.UserName, u.Email })
-            .FirstOrDefault();
+            var savedProfile = Profilerepo.GetById("acee7f97-286e-4f36-9186-607a8834aeac");
 
             Assert.IsNotNull(savedProfile);
             Assert.AreEqual("testuser4", savedProfile.UserName);
         }
 
-
         [TestMethod]
-        public async Task CanAddGeneralPostToRealDatabase()
+        public async Task CanAddLocationToDB()
         {
+            //Arrange
             using var context = new LystFiskerContext(_options);
-            //arrange get user from db
-            var savedProfile = context.Users
-            .Where(u => u.UserName == "testuser4")
-            .Select(u => new { u.UserName, u.Email })
-
-            .FirstOrDefault();
-
-            // Act: add a GeneralPost
-            var post = new GeneralPost
+            var location = new Location
             {
-                PostTime = DateTime.UtcNow,
-                Description = "Integration test post",
-                ProfileId = savedProfile.Id
+                LocationId = 3,
+                Name = "TestLocation",
+                Latitude = 10.21,
+                Longitude = 10.20,
+
             };
-            context.Posts.Add(post);
+            //Act
+            context.Locations.Add(location);
             await context.SaveChangesAsync();
 
-            // Assert: query back from the real DB
-            var savedPost = context.Posts.Include(p => p.Profile).FirstOrDefault(p => p.Description == "Integration test post");
+            var savedLocation = context.Locations.Find(location.LocationId);
 
-            Assert.IsNotNull(savedPost);
-            Assert.AreEqual("testuser", savedPost.Profile.UserName);
+            //Assert
+            Assert.IsNotNull(savedLocation);
+            Assert.AreEqual("TestLocation", savedLocation.Name);
         }
+
+        //[TestMethod]
+        //public async Task CanAddGeneralPostToDatabase()
+        //{
+        //    using var context = new LystFiskerContext(_options);
+        //    PostRepository Postrepo = new PostRepository(context);
+
+        //    //arrange get user from db
+        //    var savedProfile = context.Users
+        //    .Where(u => u.UserName == "testuser4")
+        //    .Select(u => new { u.UserName, u.Email })
+
+        //    .FirstOrDefault();
+
+        //    // Act: add a GeneralPost
+        //    var post = new GeneralPost
+        //    {
+        //        PostTime = DateTime.UtcNow,
+        //        Description = "Integration test post",
+        //        ProfileId = savedProfile.Id
+        //    };
+        //    context.Posts.Add(post);
+        //    await context.SaveChangesAsync();
+
+        //    // Assert: query back from the real DB
+        //    var savedPost = context.Posts.Include(p => p.Profile).FirstOrDefault(p => p.Description == "Integration test post");
+
+        //    Assert.IsNotNull(savedPost);
+        //    Assert.AreEqual("testuser", savedPost.Profile.UserName);
+        //}
 
     }
 }
