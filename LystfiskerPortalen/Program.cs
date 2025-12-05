a2e0c8070352338ca579d569f8d0c319168ce4fa
TheBadAngel <elisabethelida@hotmail.com>
2025-12-05 14:44:23 +0100

    Add event management feature and update navigation links
    Updated navigation links in `MainLayout.razor` to reflect new routes.
    Introduced `EventPage.razor` to display events with a card layout.
    Implemented `IEventRepository` interface and `EventRepository` class
    for event CRUD operations. Registered `IEventRepository` in DI container.
    Made minor adjustments to the solution file for cleanup.

--- LystfiskerPortalen/Program.cs
+++ LystfiskerPortalen/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddIdentityApiEndpoints<Profile>()
 
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddGeolocationServices();
 
 // Add services to the container.
